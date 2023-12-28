e083ff54b54c85b9174e4234c5ab8a7d26de4ca9
Hikakkun <htrb0130@icloud.com>
2023-12-28 11:54:44 +0900

    mainを追記

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+use virtual_stack_machine::vsm::*;
+fn main() -> io::Result<()> {
+
+    let mut vsm = Vsm::new();
+
+    vsm.read_code("/home/hikaru/prog/mini_c/virtual_stack_machine/V_1.vsm")?;
+
+    vsm.print_code();
+
+    Ok(())
 }
