229ca523b3504d09f82f7d00e75b0c7e1f08807d
Hikakkun <htrb0130@icloud.com>
2023-12-28 16:21:56 +0900

    コマンドライン引数で実行できるように変更

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,20 @@
 use std::io;
-use virtual_stack_machine::code::*;
+use virtual_stack_machine::vsm::{*, self};
+use std::env;
 
 fn main() -> io::Result<()> {
-    let mut code = Code::new();
 
-    code.read("/home/hikaru/prog/mini_c/virtual_stack_machine/tests/full.vsm")?;
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        eprintln!("Usage: {} <vsm_file>", &args[0]);
+        std::process::exit(1);
+    }
 
-    code.print();
+    let vsm_file = &args[1];
+    let mut vsm = Vsm::new();
 
+    vsm.read_code(vsm_file).unwrap();
+
+    vsm.exec_code().unwrap();
     Ok(())
 }
